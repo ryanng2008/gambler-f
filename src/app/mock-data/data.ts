b2a6ce731baa27eab5742b4e45608917f61e9c5e
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-08-19 14:47:45 +0800

    removed data.ts mock data

--- src/app/mock-data/data.ts
+++ src/app/mock-data/data.ts
@@ -1,95 +1,95 @@
-import { OptionType } from '@/app/lib/types'
-export const markets = [
-    {
-        code: "nba", 
-        name: "National Basketball Association", 
-        options: 100,
-        subsections: [
-            'lakers-warriors-10000',
-            "heat-bucks-10000"
-        ]
-    },
-    {
-        code: "nfl", 
-        name: "NFL", 
-        options: 23003430,
-        subsections: [
+// import { OptionType } from '@/app/lib/types'
+// export const markets = [
+//     {
+//         code: "nba", 
+//         name: "National Basketball Association", 
+//         options: 100,
+//         subsections: [
+//             'lakers-warriors-10000',
+//             "heat-bucks-10000"
+//         ]
+//     },
+//     {
+//         code: "nfl", 
+//         name: "NFL", 
+//         options: 23003430,
+//         subsections: [
 
-        ]
-    },
-    {
-        code: "rc-sports-day", 
-        name: "Athletics Carnival", 
-        options: 31,
-        subsections: [
+//         ]
+//     },
+//     {
+//         code: "rc-sports-day", 
+//         name: "Athletics Carnival", 
+//         options: 31,
+//         subsections: [
 
-        ]
-    }
+//         ]
+//     }
 
-]
+// ]
 
-export const subsections = [
-    {
-        code: "lakers-warriors-10000", 
-        name: "Lakers vs Warriors", 
-        details: {
-            date: '21 July',
-            time: '9:30'
-        },
-        options: [
-            '000000',
-            '000001'
-        ]
-    },
-    {
-        code: "heat-bucks-10000", 
-        name: "Heat @ Bucks", 
-        details: {
-            date: '21 July, 2021',
-            time: '8:30'
-        },
-        options: [
-            '000002',
-        ]
-    },
-]
+// export const subsections = [
+//     {
+//         code: "lakers-warriors-10000", 
+//         name: "Lakers vs Warriors", 
+//         details: {
+//             date: '21 July',
+//             time: '9:30'
+//         },
+//         options: [
+//             '000000',
+//             '000001'
+//         ]
+//     },
+//     {
+//         code: "heat-bucks-10000", 
+//         name: "Heat @ Bucks", 
+//         details: {
+//             date: '21 July, 2021',
+//             time: '8:30'
+//         },
+//         options: [
+//             '000002',
+//         ]
+//     },
+// ]
 
-export const options = [
-    {
-        id: '000000',
-        type: 'hm',
-        heading: 'C. Foley',
-        desc: 'hey',
-        imageLink: '/lepresident.png',
-        properties: {
-            bettingLine: 42.5
-        },
-        odds: 43,
-        betRange: [20, 40]
-    }, 
-    {
-        id: '000001',
-        type: 'ou',
-        heading: 'Yannick Yeung',
-        desc: '50 freestyle time',
-        imageLink: '/lepresident.png',
-        properties: {
-            bettingLine: 24.93
-        },
-        odds: 60,
-        betRange: [5, 100000000]
-    }, 
-    {
-        id: '000002',
-        type: 'ou',
-        heading: 'Yannick Ng',
-        desc: '50 crawling time',
-        imageLink: '/lepresident.png',
-        properties: {
-            bettingLine: 37
-        },
-        odds: 60,
-        betRange: [5, 20000]
-    },
-]
+// export const options = [
+//     {
+//         id: '000000',
+//         type: 'hm',
+//         heading: 'C. Foley',
+//         desc: 'hey',
+//         imageLink: '/lepresident.png',
+//         properties: {
+//             bettingLine: 42.5
+//         },
+//         odds: 43,
+//         betRange: [20, 40]
+//     }, 
+//     {
+//         id: '000001',
+//         type: 'ou',
+//         heading: 'Yannick Yeung',
+//         desc: '50 freestyle time',
+//         imageLink: '/lepresident.png',
+//         properties: {
+//             bettingLine: 24.93
+//         },
+//         odds: 60,
+//         betRange: [5, 100000000]
+//     }, 
+//     {
+//         id: '000002',
+//         type: 'ou',
+//         heading: 'Yannick Ng',
+//         desc: '50 crawling time',
+//         imageLink: '/lepresident.png',
+//         properties: {
+//             bettingLine: 37
+//         },
+//         odds: 60,
+//         betRange: [5, 20000]
+//     },
+// ]
 
