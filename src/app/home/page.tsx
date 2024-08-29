4562ca7484b7f832f1bc721a264674f2ca303df8
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-08-29 23:01:41 +0800

    pointers on homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,15 @@
+'use client'
+import { useContext } from "react"
+import AuthContext from "../context/authContext"
+import Link from "next/link"
 
 function Page() {
+  const { user } = useContext(AuthContext)
   return (
     <div className="h-[75vh]">
-        <div className="h-full my-auto flex items-center justify-center">
+        <div className="h-full my-auto flex flex-col gap-8 justify-center">
             <h1 className="text-7xl font-semibold">What&apos;s up gang!</h1>
+            {(user === null) && <Link href='/home/account' className="hover:underline hover:text-gray-600 mx-2 text-2xl">Register or login here (or press the top right button)</Link>}
         </div>
     </div>
   )
