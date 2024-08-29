da78044f045c65215a511702e562b745f28e8c17
Ryan Ng <121779670+ryanng2008@users.noreply.github.com>
2024-08-29 23:08:58 +0800

    more pointers on homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,9 +7,16 @@ function Page() {
   const { user } = useContext(AuthContext)
   return (
     <div className="h-[75vh]">
-        <div className="h-full my-auto flex flex-col gap-8 justify-center">
-            <h1 className="text-7xl font-semibold">What&apos;s up gang!</h1>
+        <div className="h-full my-auto flex flex-col gap-2 justify-center">
+            <h1 className="text-7xl font-semibold my-8">What&apos;s up gang!</h1>
             {(user === null) && <Link href='/home/account' className="hover:underline hover:text-gray-600 mx-2 text-2xl">Register or login here (or press the top right button)</Link>}
+            {(user !== null) &&
+            <>
+            <p className="text-2xl mx-2 my-2">Explore bets at the Explore tab, create bet options at the Create tab</p> 
+            <p className="text-2xl mx-2 my-2">View your bets and options in the Dashboard (inside the menu burger)</p>
+            </>
+            }
+            
         </div>
     </div>
   )
